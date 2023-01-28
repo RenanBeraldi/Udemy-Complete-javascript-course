4484aabb726693f76ad0d12bbaa6705053342e77
RenanBeraldi <renanberaldi@outlook.com>
2023-01-28 14:12:07 -0300

    var-and-values

--- 01-Fundamentals-Part-1/js/script.js
+++ 01-Fundamentals-Part-1/js/script.js
@@ -1,2 +1,40 @@
+//////////////////////////
+// Values and Variables
+//////////////////////////
+
+
+//////////////////////////
+// Values
 let js = 'amazing';
 console.log(40+8+23-10);
+
+console.log('Renan');
+console.log(21);
+
+
+//////////////////////////
+// Variables
+// camelCase notation for naming variables:
+let firstName = 'Renan';
+let first = 'Renan';
+let firstNamePerson;
+let first_name;
+
+let person;
+let PI = 3.1415;
+
+// CLEAR NAMES TO VARIABLES
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';   
+
+// NOT ALLOWED:
+// jonas&matilda
+let jonas_matilda = 'JM';
+
+// function (JS Keyword)
+let _function = 27;
+
+console.log(firstName);
