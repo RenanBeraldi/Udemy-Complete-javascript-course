cd4f2fde1979f2e2e5bfbaf7d65dd11b05232f28
RenanBeraldi <renanberaldi@outlook.com>
2023-01-28 15:49:44 -0300

    data-types

--- 01-Fundamentals-Part-1/js/script.js
+++ 01-Fundamentals-Part-1/js/script.js
@@ -1,40 +1,77 @@
-//////////////////////////
-// Values and Variables
-//////////////////////////
+// //////////////////////////
+// // Values and Variables
+// //////////////////////////
+
+
+// //////////////////////////
+// // Values
+// let js = 'amazing';
+// console.log(40+8+23-10);
+
+// console.log('Renan');
+// console.log(21);
+
+
+// //////////////////////////
+// // Variables
+// // camelCase notation for naming variables:
+// let firstName = 'Renan';
+// let first = 'Renan';
+// let firstNamePerson;
+// let first_name;
+
+// let person;
+// let PI = 3.1415;
+
+// // CLEAR NAMES TO VARIABLES
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'Programmer';
+// let job2 = 'Teacher';   
+
+// // NOT ALLOWED:
+// // jonas&matilda
+// let jonas_matilda = 'JM';
+
+// // function (JS Keyword)
+// let _function = 27;
+
+// console.log(firstName);
+
+
 
 
 //////////////////////////
-// Values
-let js = 'amazing';
-console.log(40+8+23-10);
-
-console.log('Renan');
-console.log(21);
-
-
+// DATA TYPES
 //////////////////////////
-// Variables
-// camelCase notation for naming variables:
-let firstName = 'Renan';
-let first = 'Renan';
-let firstNamePerson;
-let first_name;
 
-let person;
-let PI = 3.1415;
+// BOOLEAN
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-// CLEAR NAMES TO VARIABLES
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
 
-let job1 = 'Programmer';
-let job2 = 'Teacher';   
 
-// NOT ALLOWED:
-// jonas&matilda
-let jonas_matilda = 'JM';
+// UNDEFINED
+let year;
+console.log(year);
+console.log(typeof year)
+// an undefined variable can be reassigned
+year = 1991;
+console.log(year);
+console.log(typeof year)
 
-// function (JS Keyword)
-let _function = 27;
 
-console.log(firstName);
+
+// OPERATOR typeof
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Renan');
+
+
+
+// DYNAMIC TYPING
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun)
