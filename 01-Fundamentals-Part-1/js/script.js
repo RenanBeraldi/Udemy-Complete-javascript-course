e908eb8b374a396051bd167f906fa0d19fd136f5
RenanBeraldi <renanberaldi@outlook.com>
2023-02-02 17:57:13 -0300

    switch-statement

--- 01-Fundamentals-Part-1/js/script.js
+++ 01-Fundamentals-Part-1/js/script.js
@@ -399,20 +399,67 @@
 //////////////////////////
 // Coding Challenge #3
 //////////////////////////
-const dolphinsScoreAvg = (97+112+101) / 3;
-const koalasScoreAvg = (109+95+106) / 3;
-const minimumScore = 100;
+// const dolphinsScoreAvg = (97+112+101) / 3;
+// const koalasScoreAvg = (109+95+106) / 3;
+// const minimumScore = 100;
 
 
-console.log(`Average Dolphins Score: ${dolphinsScoreAvg}`);
-console.log(`Averaga Koalas Score: ${koalasScoreAvg}`);
+// console.log(`Average Dolphins Score: ${dolphinsScoreAvg}`);
+// console.log(`Averaga Koalas Score: ${koalasScoreAvg}`);
 
-if (dolphinsScoreAvg, koalasScoreAvg <= minimumScore){
-    console.log(`Both teams didn't get the minimum score of ${minimumScore} Therefore there is a draw!`);
-} else if (dolphinsScoreAvg === koalasScoreAvg){
-    console.log("There is a draw between the teams");
-} else if (dolphinsScoreAvg >= minimumScore && dolphinsScoreAvg > koalasScoreAvg) {
-    console.log("Dolphins are the winner of the competition! Congratulations!");
-} else if(koalasScoreAvg >= minimumScore && koalasScoreAvg > dolphinsScoreAvg) {
-    console.log("Koalas are the winner of the competition! Congratulations!");
+// if (dolphinsScoreAvg, koalasScoreAvg <= minimumScore){
+//     console.log(`Both teams didn't get the minimum score of ${minimumScore} Therefore there is a draw!`);
+// } else if (dolphinsScoreAvg === koalasScoreAvg){
+//     console.log("There is a draw between the teams");
+// } else if (dolphinsScoreAvg >= minimumScore && dolphinsScoreAvg > koalasScoreAvg) {
+//     console.log("Dolphins are the winner of the competition! Congratulations!");
+// } else if(koalasScoreAvg >= minimumScore && koalasScoreAvg > dolphinsScoreAvg) {
+//     console.log("Koalas are the winner of the competition! Congratulations!");
+// }
+
+
+
+
+
+//////////////////////////
+// Switch Statement
+//////////////////////////
+const day = "monday";
+
+switch(day){
+    case "monday":
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare theory videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Write code examples");
+        break;
+    case "friday":
+        console.log("Record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Enjoy the weekend!");
+        break;
+    default:
+        console.log("Not a valid day!")
+}
+
+if (day === "monday" ){
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+} else if (day === "tuesday"){
+    console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+    console.log("Write code examples");
+} else if (day === "friday"){
+    console.log("Record videos");
+} else if (day === "saturday" || day === "sunday"){
+    console.log("Enjoy the weekend");
+} else{
+    console.log("Not a valid day");
 }
