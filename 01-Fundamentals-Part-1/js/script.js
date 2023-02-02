2f6ea725f8483b14103041892b1500b8b136a286
RenanBeraldi <renanberaldi@outlook.com>
2023-02-02 15:15:27 -0300

    logical-operators

--- 01-Fundamentals-Part-1/js/script.js
+++ 01-Fundamentals-Part-1/js/script.js
@@ -366,3 +366,28 @@
 
 
 
+//////////////////////////
+// Logical Operators
+//////////////////////////
+// const hasDriversLicense = true; //A
+// const hasGoodVision = true; //B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// // if(hasDriversLicense && hasGoodVision){
+// //     console.log("Sarah is able to drive!");
+// // } else {
+// //     console.log("Someone else should drive!");
+// // }
+
+// const isTired = true; //C
+
+//  console.log(hasDriversLicense || hasGoodVision || isTired);
+
+//  if(hasDriversLicense && hasGoodVision && !isTired){
+//     console.log("Someone else should drive!");
+// } else {
+//     console.log("Sarah is able to drive!");
+// }
