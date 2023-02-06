0c6f542728bf6db4593a72f5b2f3ec8750c803df
RenanBeraldi <renanberaldi@outlook.com>
2023-02-06 14:24:48 -0300

    functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,13 +1,41 @@
 //////////////////////////
 // STRICT MODE 
 //////////////////////////
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("You can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("You can drive");
 
 // const interface = 'Audio';
 // const private = 534;
+
+
+
+
+
+//////////////////////////
+// FUNCTION
+//////////////////////////
+function logger(){
+    console.log("My name is Renan");
+}
+
+// calling / running / involking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice); 
