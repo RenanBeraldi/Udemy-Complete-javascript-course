a4049096349d375a62d72ccd758bc42214c519d9
RenanBeraldi <renanberaldi@outlook.com>
2023-02-06 09:13:03 -0300

    section1-ended

--- 01-Fundamentals-Part-1/js/script.js
+++ 01-Fundamentals-Part-1/js/script.js
@@ -494,7 +494,7 @@
 //////////////////////////
 // CODING CHALLENGE #4
 //////////////////////////
-const bill = 275;
+// const bill = 275;
 
-const tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2;
-console.log(`The bill is $${bill}, the tip is $${tip}, and the total value is $${bill+tip}`)
+// const tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2;
+// console.log(`The bill is $${bill}, the tip is $${tip}, and the total value is $${bill+tip}`)
