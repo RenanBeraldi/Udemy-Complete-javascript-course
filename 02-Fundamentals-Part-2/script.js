15d988dbe16d2cc5087bc03760f9f2f41ff88c91
RenanBeraldi <renanberaldi@outlook.com>
2023-02-06 12:00:08 -0300

    strict-mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,13 @@
+//////////////////////////
+// STRICT MODE 
+//////////////////////////
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("You can drive");
+
+// const interface = 'Audio';
+// const private = 534;
