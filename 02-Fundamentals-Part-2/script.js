ffc54471d11ed03de4decafe81edcf48304f7301
RenanBeraldi <renanberaldi@outlook.com>
2023-02-07 09:44:59 -0300

    reviewing-functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -86,16 +86,43 @@
 //////////////////////////
 // FUNCTIONS CALLING OTHER FUNCTIONS
 //////////////////////////
-function cutFruitPieces(fruit){
-    return fruit * 4;
+// function cutFruitPieces(fruit){
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} apples pieces and ${orangePieces} oranges pieces.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+
+
+
+
+//////////////////////////
+// REVIEWING FUNCTIONS
+//////////////////////////
+const calcAge = function(birthYear){
+    return 2023 - birthYear;
+}  
+
+const yearsUntilRetirement = function (birthYear, firstName){
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years!`);
+        return retirement;
+    } else{
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+    // 
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-    
-    const juice = `Juice with ${applePieces} apples pieces and ${orangePieces} oranges pieces.`;
-    return juice;
-}
-
-console.log(fruitProcessor(2, 3))
+console.log(yearsUntilRetirement(2002, "Renan"));
+console.log(yearsUntilRetirement(1950, "Mike"));    
