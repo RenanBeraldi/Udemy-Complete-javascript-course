6b2f5d775fa11137c0c3f72b8b11ea7e35d3e322
RenanBeraldi <renanberaldi@outlook.com>
2023-03-10 13:06:21 -0300

    Hoisting-TDZ-practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,35 +1,87 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = currentYear - birthYear;
+///////////////////////////////
+// Scoping in practice
+///////////////////////////////
+// function calcAge(birthYear) {
+//   const age = currentYear - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's varible
-      const firstName = 'Steven';
-      
-      // Reassigning outer scope's variable
-      output = 'NEW OUTPUT';
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's varible
+//       const firstName = 'Steven';
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    console.log(millenial);
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
+//       // Reassigning outer scope's variable
+//       output = 'NEW OUTPUT';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-const currentYear = new Date().getFullYear();
-const firstName = 'Renan';
-calcAge(2002);
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const currentYear = new Date().getFullYear();
+// const firstName = 'Renan';
+// calcAge(2002);
+
+///////////////////////////////
+// Hoisting and TDZ in practice
+///////////////////////////////
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Renan';
+// let job = 'Programmer';
+// const year = 2002;
+
+// // Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+
+// // Examples
+
+// if (!numProducts) {
+//   deleteShoppingCart();
+// }
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+///////////////////////////////
+// this keyword in practice
+///////////////////////////////
