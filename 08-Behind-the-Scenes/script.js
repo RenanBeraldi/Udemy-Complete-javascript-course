fe550a0315253188ce0e4198d85ba49179d186f6
RenanBeraldi <renanberaldi@outlook.com>
2023-03-13 16:10:27 -0300

    this-keyword-in-practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -65,7 +65,6 @@
 
 // var addArrow = (a, b) => a + b;
 
-
 // // Examples
 
 // if (!numProducts) {
@@ -85,3 +84,35 @@
 ///////////////////////////////
 // this keyword in practice
 ///////////////////////////////
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(2002);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(2002);
+
+const renan = {
+  year: 2002,
+  calcAge: function () {
+    console.log(this);
+    console.log(new Date().getFullYear() - this.year);
+  },
+};
+renan.calcAge();
+
+const matilda = {
+    year: 2017,
+}
+
+matilda.calcAge = renan.calcAge;
+matilda.calcAge();
+
+const f = renan.calcAge;
+f();
