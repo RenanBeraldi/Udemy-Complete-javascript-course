5a22022316f900c1ba86da4ff9f6e6d67aa1fc62
RenanBeraldi <renanberaldi@outlook.com>
2023-03-13 21:02:32 -0300

    regularFuctions-vs-arrowFunctions

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -84,35 +84,88 @@
 ///////////////////////////////
 // this keyword in practice
 ///////////////////////////////
-console.log(this);
+// console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(2002);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(2002);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(2002);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(2002);
+
+// const renan = {
+//   year: 2002,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(new Date().getFullYear() - this.year);
+//   },
+// };
+// renan.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// }
+
+// matilda.calcAge = renan.calcAge;
+// matilda.calcAge();
+
+// const f = renan.calcAge;
+// f();
+
+///////////////////////////////
+// Regular Functions vs. Arrow Functions
+///////////////////////////////
+
+// var firstName = 'Matilda';
 
 const renan = {
+  firstName: 'Renan',
   year: 2002,
   calcAge: function () {
     console.log(this);
     console.log(new Date().getFullYear() - this.year);
+
+    // Solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 || this.year <= 1996);
+    // };
+    // isMillenial();
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey, ${this.firstName}`);
   },
 };
+renan.greet();
 renan.calcAge();
 
-const matilda = {
-    year: 2017,
-}
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-matilda.calcAge = renan.calcAge;
-matilda.calcAge();
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
 
-const f = renan.calcAge;
-f();
+addArrow(2, 5, 8);
