cd1bc9d15507563300d57d30510f5c662b166945
RenanBeraldi <renanberaldi@outlook.com>
2023-03-22 09:31:30 -0300

    coding-challenge1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -52,37 +52,107 @@ const restaurant = {
   },
 };
 ////////////////////////////////////////
+// Coding Challenge #1
+////////////////////////////////////////
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Task 1
+const [players1, players2] = game.players;
+
+// Task 2
+const [gk, ...fieldPlayers] = players1;
+
+// Task 3
+const [allPlayers] = [...players1, ...players2];
+
+// Task 4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// Task 5
+const { team1, x: draw, team2 } = game.odds;
+
+// Task 6
+const printGoals = function (...players) {
+  console.log(`${players.length} goals scored`);
+};
+printGoals(...game.scored);
+
+// Task 7
+team1 < team2 && console.log('Team 1 will probably win');
+team2 < team1 && console.log('Team 2 will probably win');
+
+////////////////////////////////////////
 // Logical Assignment Operators
 ////////////////////////////////////////
 
-const rest1 = {
-  name: 'Capri',
-  numGuests: 0,
-};
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 0,
+// };
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
 
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
+// // OR assignment operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests ||= 10;
+// // rest2.numGuests ||= 10;
 
-// NULLISH assignment operator
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// // NULLISH assignment operator
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-// AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// // AND assignment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 ////////////////////////////////////////
 // The Nullish Coalescing Operator (??)
