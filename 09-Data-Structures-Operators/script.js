0166fc1a9cb22fa27b4f97deb4c473e0f0a9a706
RenanBeraldi <renanberaldi@outlook.com>
2023-04-17 14:19:11 -0300

    Optional Chaining Operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -14,7 +14,7 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -51,6 +51,38 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+////////////////////////////////////////
+// Optional Chaining
+////////////////////////////////////////
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// WITH Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  // console.log(`On ${day}, we open at ${open}`);
+  if (open === 'closed') {
+    console.log(`On ${day}, we are closed`);
+  } else {
+    console.log(`On ${day}, we open at ${open}`);
+  }
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); // Method does not exist
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(users[0]?.name ?? 'User array empty');
 
 ////////////////////////////////////////
 // Looping Arrays: The for-of loop
