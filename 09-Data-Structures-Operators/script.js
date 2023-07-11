37b08d65a56269ca160a9ee99c2dd33db771e0bb
RenanBeraldi <renanberaldi@outlook.com>
2023-07-10 21:15:14 -0300

    Maps Fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -52,6 +52,53 @@ const restaurant = {
 ////////////////////////////////////////
 // Maps: Fundamentals
 ////////////////////////////////////////
+// A Map is data structure that we can use to map values to keys.
+// The big difference between objects and maps is that in maps, the keys can have any type. So, in objects, the keys are basically always strings. But in maps, we can have any type of key. It could even be objects, or arrays, or other maps.
+
+// const rest = new Map();
+// // The easiest way to create a map is to actually create an empty map.
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+// // Calling the set method like this does not only update the map that it's called on, but also returns the map.
+
+// // The fact that the set method actually returns the updated map, allows us to chain the set method like this:
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed');
+
+// // In order to read data from a map, we use the 'get' method. And so, all we need to do is to pass in, the name of the key.
+// console.log(rest.get('name')); // Classico Italiano
+// console.log(rest.get(true)); // We are open :D
+// console.log(rest.get(1)); // Firenze, Italy
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// // This operation checks if the var time is between 11 and 23, if it is, it will return a boolean value (true), returning the string 'We are open :D'
+// // This operation is not so readable. Don't overuse this kind of pattern.
+
+// // To check if a map contains a certain key.
+// console.log(rest.has('categories'));
+
+// // We can also delete elements from the map.
+// rest.delete(2);
+// console.log(rest);
+
+// // Maps also have the size property.
+// console.log(rest.size);
+
+// // We can also clear, basically, remove all the elements from the Map.
+// // rest.clear();
+
+// // We can in fact use arrays or objects as map keys.
+// const arr = [1, 2];
+// // The result of requesting this array, we get undefined. And the reason for that, is that these two arrays (rest.set([1, 2], 'Test')) are actually not the same object.
+// // In order to make it work, we would have to declare a variable and use it on set and get.
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
 
 ////////////////////////////////////////
 // Sets
@@ -110,70 +157,70 @@ BONUS: Create an object called 'scorers' which contains the names of the players
     } 
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
 
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-// Task 1
-for (const [sIndex, sValue] of game.scored.entries()) {
-  console.log(`Goal ${sIndex + 1}: ${sValue}`);
-}
+// // Task 1
+// for (const [sIndex, sValue] of game.scored.entries()) {
+//   console.log(`Goal ${sIndex + 1}: ${sValue}`);
+// }
 
-// Task 2
-let avgOdd = 0;
-let sum = 0;
-for (const odd of Object.values(game.odds)) {
-  sum += odd;
-  avgOdd = sum / Object.keys(game.odds).length;
-}
-console.log(`This is the avg Odd of the game ${avgOdd}`);
+// // Task 2
+// let avgOdd = 0;
+// let sum = 0;
+// for (const odd of Object.values(game.odds)) {
+//   sum += odd;
+//   avgOdd = sum / Object.keys(game.odds).length;
+// }
+// console.log(`This is the avg Odd of the game ${avgOdd}`);
 
-// Task 3
-for (const [team, odd] of Object.entries(game.odds)) {
-  let str =
-    team != 'x'
-      ? `Chance of victory ${game[team]}: ${odd}`
-      : `Chance of draw: ${odd}`;
-  console.log(str);
-}
+// // Task 3
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   let str =
+//     team != 'x'
+//       ? `Chance of victory ${game[team]}: ${odd}`
+//       : `Chance of draw: ${odd}`;
+//   console.log(str);
+// }
 
 ////////////////////////////////////////
 // Looping Objects: Object Keys, Values, Entries
