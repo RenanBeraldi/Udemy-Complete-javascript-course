c2e0f9c7ab440c85b4152566d83a0f90e62150ed
RenanBeraldi <renanberaldi@outlook.com>
2023-07-22 14:44:35 -0300

    first class and higher order functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,45 +1,77 @@
 'use strict';
 ////////////////////////////////
+// First-Class and Higher-Order Functions
+////////////////////////////////
+
+// JavaScript is a language that has First-Class functions, which in technical terms means that functions are so-called first citizens. In practice, that means that functions are simply treated as values. Why does JS works this way? It's simply because functions are really just another type of objects in JS. And since objects are values, functions are values too.
+
+// And since functions are values, there is a bunch of interesting things that we can do with them, like storing them in variables or objects properties.
+// We can also pass functions as arguments to other functions.
+// We can return functions from another functions.
+
+// Since functions are objects, many type of objects in JS have methods, like array methods. There are also function methods, methods that we can call on functions.
+
+// The fact that JS has first-class functions, makes it possible for us to use and write higher-order functions
+
+// A higher-order function is either a function that receives another function as an argument, or a function that returns a new function.
+
+/* 1. Function that receives another function:
+const greet = () => console.log('Hey, Renan');
+btnClose.addEventListener('click', greet); */
+
+// Here the addEventListener is the Higher-Order function, well, because it receives another function as an input, the greet function. And we call the function that is passed in is a callback function. That's because the callback function will be called later by the higher-order function.
+
+/* 2. Function that returns new function:
+function count() {
+    let counter = 0;
+    return function () {
+        counter++;
+    };
+} */
+
+// First-Class functions is just a feature that a programming language either has or does not have. All that means is that all functions are values. All it means is that all functions are values. There are however Higher-Order functions in practice, which is possible because the language supports first class functions. 
+
+////////////////////////////////
 // How Passing Arguments Works: Value vs. Reference
 ////////////////////////////////
 
-const flight = 'LH234';
-const renan = {
-  name: 'Renan Beraldi',
-  passport: 478468764876,
-};
+// const flight = 'LH234';
+// const renan = {
+//   name: 'Renan Beraldi',
+//   passport: 478468764876,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 478468764876) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport');
-  }
-};
+//   if (passenger.passport === 478468764876) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
 
+// // checkIn(flight, renan);
+// // console.log(flight);
+// // console.log(renan);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = renan; // When we try to copy an object like this, we are only copying the reference to that object in the memory heap, but they both point to the same object object in memory.
+
+// // In summary, passing a primitive type to a function is really just the same as creating a copy like this "const flightNum = flight", outside of the function.
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+
+// newPassport(renan);
 // checkIn(flight, renan);
-// console.log(flight);
-// console.log(renan);
-
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = renan; // When we try to copy an object like this, we are only copying the reference to that object in the memory heap, but they both point to the same object object in memory.
-
-// In summary, passing a primitive type to a function is really just the same as creating a copy like this "const flightNum = flight", outside of the function.
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000);
-};
-
-newPassport(renan);
-checkIn(flight, renan);
 
 // There are two term thar are used all the time when dealing with functions, which is passing by value, and passing by reference.
-// JavaScript does not have passing by reference, only passing by value, even though it looks like it's passing by reference. Passing by reference is like you could pass a reference to the value of five, and then the original value, outside of the function, would be changed.  
-// For objects, we do in fact pass in a reference. So the memory address of the object. However, that reference itself is still a value. It's simply a value that contains a memory address. So, basically, we pass a reference to the function, but we do not pass by reference. 
+// JavaScript does not have passing by reference, only passing by value, even though it looks like it's passing by reference. Passing by reference is like you could pass a reference to the value of five, and then the original value, outside of the function, would be changed.
+// For objects, we do in fact pass in a reference. So the memory address of the object. However, that reference itself is still a value. It's simply a value that contains a memory address. So, basically, we pass a reference to the function, but we do not pass by reference.
 
 ////////////////////////////////
 // Default Parameters
