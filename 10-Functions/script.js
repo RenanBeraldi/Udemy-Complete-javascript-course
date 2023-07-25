69f6235c12855c62219c475db1a91b58cb8f04a6
RenanBeraldi <renanberaldi@outlook.com>
2023-07-24 23:46:54 -0300

    Immediately Invoked Function Expression

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,99 +1,128 @@
 'use strict';
 ////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+////////////////////////////////
+// Sometimes in JS, we need a function that is only executed once. Basically a function that disappears right after it's called once.
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+(function () {
+  console.log('This will never run again');
+})(); // This pattern is called IIFE
+// Wrapping into parenthesis, we basically transformed the statement that we had before into an expression.
+
+// The same work for arrow function.
+(() => console.log('This will ALSO never run again'))();
+
+// Why was this pattern actually invented?
+// We already know that functions create scopes. What's important here, is that one scope does not have access to variables from an inner scope. We also say that data defined inside a scope is private, we also say that this data is encapsulated.
+// Data encapsulation and data privacy are extremely important concepts in programming. So many times we actually need to protect our variables, from being accidentally overwritten by some other parts of the program, or even external scripts or libraries.
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+
+////////////////////////////////
 // The bind Method
 ////////////////////////////////
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a set on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a set on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-lufthansa.book(239, 'Renan Beraldi');
-lufthansa.book(635, 'John Smith');
+// lufthansa.book(239, 'Renan Beraldi');
+// lufthansa.book(635, 'John Smith');
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
-book.call(eurowings, 23, 'Sarah Williams');
-
-book.call(lufthansa, 239, 'Mary Cooper');
-
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-
-book.call(swiss, 583, 'Mary Cooper');
-console.log(swiss);
-
-const flightData = [583, 'George Cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-book.call(swiss, ...flightData);
-
-// just like the call Method, bind also allows us to manually set the this keyword for any function call. Now, the difference is that bind does not immediately call the function. Instead, it returns a new function where the this keyword is bound. So it's set to whatever value we pass into bind.
 // book.call(eurowings, 23, 'Sarah Williams');
 
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-bookEW(23, 'Steven Williams'); // This function already has the this keyword set in stone basically. We no longer need to specify the this keyword again.
+// book.call(lufthansa, 239, 'Mary Cooper');
 
-// In the call method, we can pass multiple arguments besides the this keyword. And so in the bind method, we can actually do the same. And then all of these arguments will also be basically set in stone. So they will be defined and the function will then always be called with these same arguments. For example, we could use bind to create a function for one specific airline and a specific flight number.
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
 
-const bookEW23 = book.bind(eurowings, 23); // This function now only needs the name, cause it was already preset with the flightNum
-bookEW23('Renan Beraldi');
-bookEW23('Martha 23');
+// book.call(swiss, 583, 'Mary Cooper');
+// console.log(swiss);
 
-// Specifying parts of the argument beforehand, is actually a common pattern called partial application. So essentially, partial application means that a part of the arguments of the original function are already applied, which means, already set.
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
 
-// There are more situations where the bind method is really useful. And one example of that is when we use objects together with event listeners.
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
+// book.call(swiss, ...flightData);
 
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// // just like the call Method, bind also allows us to manually set the this keyword for any function call. Now, the difference is that bind does not immediately call the function. Instead, it returns a new function where the this keyword is bound. So it's set to whatever value we pass into bind.
+// // book.call(eurowings, 23, 'Sarah Williams');
 
-// Partial Application
-// And in this case of partial application, many times we are not even interested in this keyword, but we still use bind for this.
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+// bookEW(23, 'Steven Williams'); // This function already has the this keyword set in stone basically. We no longer need to specify the this keyword again.
 
-// This here is the general function for adding tax. But now let's say that there is one tax that we use all the time.
-const addVAT = addTax.bind(null, 0.23);
-// addVAT = value => value + value * rate;
-console.log(addVAT(100));
-console.log(addVAT(23));
-// This is creating a more specific function based on a more general function.
+// // In the call method, we can pass multiple arguments besides the this keyword. And so in the bind method, we can actually do the same. And then all of these arguments will also be basically set in stone. So they will be defined and the function will then always be called with these same arguments. For example, we could use bind to create a function for one specific airline and a specific flight number.
 
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
+// const bookEW23 = book.bind(eurowings, 23); // This function now only needs the name, cause it was already preset with the flightNum
+// bookEW23('Renan Beraldi');
+// bookEW23('Martha 23');
 
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
+// // Specifying parts of the argument beforehand, is actually a common pattern called partial application. So essentially, partial application means that a part of the arguments of the original function are already applied, which means, already set.
+
+// // There are more situations where the bind method is really useful. And one example of that is when we use objects together with event listeners.
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial Application
+// // And in this case of partial application, many times we are not even interested in this keyword, but we still use bind for this.
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// // This here is the general function for adding tax. But now let's say that there is one tax that we use all the time.
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value * rate;
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+// // This is creating a more specific function based on a more general function.
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+// console.log(addVAT2(23));
 
 ////////////////////////////////
 // The Call and Apply Methods.
